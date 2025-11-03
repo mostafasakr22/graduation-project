07159d96f0564109309a272ce3e1dd7ad50a725f
Mostafa Sakr <mosakr1422@gmail.com>
2025-11-03 02:31:27 -0800

    start_BlackBox

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,22 +12,11 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'name','email','password','role',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
