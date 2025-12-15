e1e52e0c402f73011e1968a0fa134bdced0f0bba
Mostafa Sakr <mosakr1422@gmail.com>
2025-12-16 01:06:32 +0200

    update vehicle table

--- database/migrations/2025_10_27_101424_create_vehicles_table.php
+++ database/migrations/2025_10_27_101424_create_vehicles_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
         Schema::create('vehicles', function (Blueprint $table) {
             $table->id();
             $table->foreignId('driver_id')->constrained('drivers')->onDelete('cascade')->unique();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('no action');
             $table->string('make');
             $table->string('model');
             $table->string('plate_number')->unique();
