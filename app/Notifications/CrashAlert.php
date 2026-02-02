1c0118c1ad68c6964e9a04d049978e1471cb8e6b
Mostafa Sakr <mosakr1422@gmail.com>
2026-02-02 03:42:21 +0200

    update crash alert

--- app/Notifications/CrashAlert.php
+++ app/Notifications/CrashAlert.php
@@ -2,6 +2,7 @@
 
 namespace App\Notifications;
 
+use App\Models\Crash;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
 
@@ -9,6 +10,7 @@ class CrashAlert extends Notification
 {
     use Queueable;
 
+    public $crash;
     public $crashData; // بيانات الحادثة
 
     public function __construct($crashData)
@@ -25,12 +27,15 @@ class CrashAlert extends Notification
     // شكل البيانات اللي هتتخزن في الداتابيز
     public function toDatabase($notifiable)
     {
+        
+    $vehicleInfo = $this->crash->vehicle->plate_number ?? 'ID #' . $this->crash->vehicle_id;
         return [
-            'title'   => '🚨 Crash Detected!',
-            'body'    => 'Vehicle ' . $this->crashData['plate_number'] . ' reported a crash.',
-            'type'    => 'crash',
-            'id'      => $this->crashData['id'], 
-            'time'    => now()
+            'title'        => '🚨 Crash Detected!',
+            'body'         => "Vehicle ({$vehicleInfo}) reported a crash.",
+            'crash_id'     => $this->crash->id,
+            'severity'     => $this->crash->severity,
+            'time'         => now(),
+            'icon'         => 'crash_alert'
         ];
     }
 }
