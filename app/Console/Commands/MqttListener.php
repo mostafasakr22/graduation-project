d105370033ac72c8a7dba4ba19d0a1ce5b7ed185
Mostafa Sakr <mosakr1422@gmail.com>
2026-04-22 19:43:04 +0200

    update

--- app/Console/Commands/MqttListener.php
+++ app/Console/Commands/MqttListener.php
@@ -4,7 +4,6 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use PhpMqtt\Client\Facades\MQTT;
-// امسح سطر الـ use القديم بتاع Mqtt وجرب تناديه بالمسار الكامل تحت
 
 class MqttListener extends Command
 {
@@ -15,7 +14,6 @@ class MqttListener extends Command
     {
         $this->info('Waiting for data...');
 
-        // استخدمنا المسار الكامل هنا عشان نهرب من أي مشكلة في الـ discovery
         $mqtt = MQTT::connection();
         
         $topic = 'graduation/test/data';
